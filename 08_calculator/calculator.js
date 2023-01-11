2f94b47f8f7b51c3b78560fb454ba00026e60c50
RASH1k-RAJ <pommyraj@gmail.com>
2023-01-11 13:23:17 +1100

    did the calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total+= item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total *= item, 1);
 };
 
-const power = function() {
-	
+const power = function(n, pow){
+  if(pow == 0) return 1;
+  let prod = n;
+	for(let i=0; i<pow -1; i++){
+    n*=prod;
+  }
+  return n;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n=== 0 || n===1) return 1;
+  let factorial = 1;
+  while(n > 0){
+    factorial *= n;
+    n -= 1;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
