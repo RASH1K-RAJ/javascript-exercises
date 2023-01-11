29a5069193ad711a1ef8f3fd488b454cfdeb7404
RASH1k-RAJ <pommyraj@gmail.com>
2023-01-11 13:58:55 +1100

    Did the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let symbols = words.toLowerCase().split('');
+    const letters = symbols.filter((symbol) => 'a' <= symbol && symbol <= 'z');
+    return letters.toString() === letters.reverse().toString();
 };
+
+//THE WAY THEY DID IT: 
+// const palindromes = function(string) {
+//     const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+//     return (
+//       processedString
+//         .split("")
+//         .reverse()
+//         .join("") == processedString
+//     );
+//   };
 
 // Do not edit below this line
 module.exports = palindromes;
