fab571f28fab74062e677b1a07afaafb8d6851ac
RASH1k-RAJ <pommyraj@gmail.com>
2023-01-11 14:35:31 +1100

    findished 'findTheOldest' exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach((person) => {
+       if(!(person.yearOfDeath)) person.yearOfDeath = (new Date()).getFullYear();
+    });
+    const oldToYoung = people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)? -1: 1);
+    return oldToYoung[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
