0833027b9bbf9b8b3ff0553672425aeb70c2b6e1
RASH1k-RAJ <pommyraj@gmail.com>
2023-01-11 14:17:31 +1100

    completed the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    count = parseInt(count);
+    if(count<1) return 'OOPS';
+    if(count == 0) return 0;
 
+    let a = 0;
+    let b = 1;
+
+    for(let i = 0; i< count; i++){
+        const temp = b;
+        b = b + a;
+        a = temp;
+    }
+    return b;s
 };
 
 // Do not edit below this line
